1c63b95afadc1e39c01baf3b1d97c905e5d68dc2
Kishor Dhruv <mm23b046@smail.iitm.ac.in>
2026-02-03 10:29:26 +0530

    Removed the pydantic validator or user input  user_input.py

--- Api/schema/user_input.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Annotated
-
-
-class UserInput(BaseModel):
-
-    formula : Annotated[str, Field(..., description='It is a material chemical formula')]
-
-    
